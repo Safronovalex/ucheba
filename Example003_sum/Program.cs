186ea518c1e481c92b20ce2545db2d6962a0d7b3
Safronov <safronovalex0205@yandex.ru>
2022-06-08 23:07:57 +0300

    new random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int numberA = 555;
-int numberB = 111;
+﻿int numberA = new Random().Next(5,15);
+Console.WriteLine(numberA);
+int numberB = 1;
 int result = numberA + numberB;
 Console.WriteLine(result);
 
